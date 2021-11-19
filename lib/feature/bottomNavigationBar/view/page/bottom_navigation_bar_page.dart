8759b3f25b9ec6c173680b5cf0a7e486c9cd66b2
farukmeral25 <farukmeral25@gmail.com>
2021-11-20 02:14:11 +0300

    -Dosyalama ve widget eklendi.

--- lib/feature/bottomNavigationBar/view/page/bottom_navigation_bar_page.dart
+++ lib/feature/bottomNavigationBar/view/page/bottom_navigation_bar_page.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/core/constant/theme/appTheme/app_color.dart';
-import 'package:flutter_clean_architecture/core/constant/theme/appTheme/app_text_style.dart';
+import 'package:flutter_clean_architecture/feature/bottomNavigationBar/view/widgets/bottom_navigation_bar.dart';
 import 'package:flutter_clean_architecture/feature/bottomNavigationBar/viewmodel/bottom_navigation_bar_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -22,81 +22,7 @@ class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
           appBar: bottomNavigationBarProvider.appbars[bottomNavigationBarProvider.currentIndex],
           body: bottomNavigationBarProvider.bodies[bottomNavigationBarProvider.currentIndex],
           backgroundColor: AppColor.skin,
-          bottomNavigationBar: ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            child: Container(
-              height: 75,
-              width: MediaQuery.of(context).size.width,
-              color: AppColor.white,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        bottomNavigationBarProvider.currentIndex = 0;
-                        bottomNavigationBarProvider.visible = true;
-                      },
-                      child: Container(
-                        height: 75,
-                        width: (MediaQuery.of(context).size.width - 50) / 2,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              'Jobs',
-                              style: AppTextStyle.body14w700.copyWith(
-                                color: bottomNavigationBarProvider.visible ? AppColor.black : AppColor.grey,
-                              ),
-                            ),
-                            Visibility(
-                              visible: bottomNavigationBarProvider.visible,
-                              child: CircleAvatar(
-                                backgroundColor: AppColor.black,
-                                radius: 4,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        bottomNavigationBarProvider.currentIndex = 1;
-                        bottomNavigationBarProvider.visible = false;
-                      },
-                      child: Container(
-                        height: 75,
-                        width: (MediaQuery.of(context).size.width - 50) / 2,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              'Applications',
-                              style: AppTextStyle.body14w700.copyWith(
-                                color: !bottomNavigationBarProvider.visible ? AppColor.black : AppColor.grey,
-                              ),
-                            ),
-                            Visibility(
-                              visible: !bottomNavigationBarProvider.visible,
-                              child: CircleAvatar(
-                                backgroundColor: AppColor.black,
-                                radius: 4,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          bottomNavigationBar: BottomNavigationBarWidget(),
         );
       },
     );
